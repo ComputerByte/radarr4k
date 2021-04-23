5b5a81b12b6d4412a00f7577e232f6969d394ff7
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2021-04-23 00:37:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 echo Hello, input your swizzin username or this will break.
 read varname
-echo Installing mono
-apt install apt-transport-https dirmngr gnupg ca-certificates curl mediainfo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/debian stable-buster main" | tee /etc/apt/sources.list.d/mono-official-stable.list
-apt update
-apt install mono-devel
+echo Installing mono dependencies
+apt install apt-transport-https dirmngr gnupg ca-certificates curl mediainfo > /dev/null 2>&1
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1
+echo "deb https://download.mono-project.com/repo/debian stable-buster main" | tee /etc/apt/sources.list.d/mono-official-stable.list > /dev/null 2>&1
+apt update > /dev/null 2>&1
+echo installing mono
+apt install mono-devel > /dev/null 2>&1
+echo Making directories
 mkdir /home/$varname/scripts
 cd /home/$varname/scripts
-wget https://github.com/Radarr/Radarr/releases/download/v3.0.2.4552/Radarr.master.3.0.2.4552.linux-core-x64.tar.gz
-tar -xf Radarr*
-chown -R $varname:$varname /home/$varname/scripts/Radarr
-#Stop Radarr
+echo Downloading Radarr
+wget https://github.com/Radarr/Radarr/releases/download/v3.0.2.4552/Radarr.master.3.0.2.4552.linux-core-x64.tar.gz  > /dev/null 2>&1
+tar -xf Radarr*  > /dev/null 2>&1
+chown -R $varname:$varname /home/$varname/scripts/Radarr  > /dev/null 2>&1
 systemctl stop radarr.service
-wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/radarr4k.service
-sed -i "s/swizz/$varname/g" /home/$varname/scripts/radarr4k.service
-wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/radarr4k.conf
-sed -i "s/swizz/$varname/g" /home/$varname/scripts/radarr4k.conf
-mv radarr4k.service /etc/systemd/system
-systemctl enable radarr4k.service
-systemctl start radarr4k.service
+echo Downloading service files
+wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/radarr4k.service > /dev/null 2>&1
+sed -i "s/swizz/$varname/g" /home/$varname/scripts/radarr4k.service > /dev/null 2>&1
+wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/radarr4k.conf > /dev/null 2>&1
+sed -i "s/swizz/$varname/g" /home/$varname/scripts/radarr4k.conf > /dev/null 2>&1
+mv radarr4k.service /etc/systemd/system > /dev/null 2>&1
+echo Starting Radarr4k for config creation
+systemctl enable radarr4k.service > /dev/null 2>&1
+systemctl start radarr4k.service > /dev/null 2>&1
 echo Sleep For 10 seconds
 sleep 10
-systemctl stop radarr4k.service
-systemctl stop radarr.service
-sed -i "s/7878/9000/g" /home/$varname/config/Radarr4k/config.xml
-sed -i "s/<UrlBase><\/UrlBase>/<UrlBase>\/radarr4k<\/UrlBase>/g" /home/$varname/config/Radarr4k/config.xml
+echo Stop radarr4k
+systemctl stop radarr4k.service > /dev/null 2>&1
+systemctl stop radarr.service > /dev/null 2>&1
+echo Modifying radarr4k configs to not conflict with radarr
+sed -i "s/7878/9000/g" /home/$varname/config/Radarr4k/config.xml > /dev/null 2>&1
+sed -i "s/<UrlBase><\/UrlBase>/<UrlBase>\/radarr4k<\/UrlBase>/g" /home/$varname/config/Radarr4k/config.xml > /dev/null 2>&1
+echo Installing nginx profiles
 mv /home/$varname/scripts/radarr4k.conf /etc/nginx/apps
-nginx -t && nginx -s reload
-systemctl start radarr.service
+nginx -t && nginx -s reload > /dev/null 2>&1
+echo Starting radarr
+systemctl start radarr.service > /dev/null 2>&1
 echo Sleep 10
 sleep 10
-systemctl start radarr4k.service
+echo Starting radarr4k
+systemctl start radarr4k.service > /dev/null 2>&1
+echo Installing swizzin profile for panel
 cd /home/$varname/scripts
-wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/profiles.py
-mv /opt/swizzin/core/custom/profiles.py /opt/swizzin/core/custom/profiles.py.backup
-cp profiles.py /opt/swizzin/core/custom
-touch /install/.radarr4k.lock
-systemctl restart panel
+wget https://raw.githubusercontent.com/ComputerByte/radarr4k/main/profiles.py  > /dev/null 2>&1
+mv /opt/swizzin/core/custom/profiles.py /opt/swizzin/core/custom/profiles.py.backup  > /dev/null 2>&1
+cp profiles.py /opt/swizzin/core/custom  > /dev/null 2>&1
+touch /install/.radarr4k.lock  > /dev/null 2>&1
+systemctl restart panel  > /dev/null 2>&1
